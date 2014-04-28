4ad03346fede93808c48950b6b91e12b678ea545
Boris <boris.kvid@sitepointgroup.com>
2014-04-28 12:43:04 +1000

    SP design pluginification: moving to an updated stylesheet inclusion API

--- plugin.rb
+++ plugin.rb
@@ -30,9 +30,6 @@ after_initialize do
 
 end
 
-# TODO: this file needs to be loaded instead of app/assets/stylesheets/common/foundation/variables.scss
-# register_asset "stylesheets/common/foundation/variables.scss"
-
 # register_asset "javascripts/admin/templates/site_settings.js.handlebars" # doesn't work, but probably not needed (Admin section)
 register_asset "javascripts/discourse/components/categorydrop_component.js" # ok
 register_asset "javascripts/discourse/components/sortable_heading_component.js" # ok
@@ -50,6 +47,8 @@ register_asset "javascripts/discourse/views/post_menu_view.js" # ok
 register_asset "javascripts/discourse/views/post_view.js" # ok
 register_asset "javascripts/discourse/views/activity_filter_view.js" # ok
 
+
+register_asset("stylesheets/common/foundation/variables.scss", :variables)
 register_asset "stylesheets/common/components/badges.css.scss"
 register_asset "stylesheets/common/components/buttons.scss"
 register_asset "stylesheets/common/components/navs.scss"
@@ -62,68 +61,3 @@ register_asset "stylesheets/desktop/topic.scss"
 register_asset "stylesheets/desktop/user.scss"
 
 register_asset "stylesheets/sitepoint/typography.scss"
-
-
-
-# // $base-font-family: "Times", "Helvetica Neue", Helvetica, Arial, sans-serif;
-
-# // // OUR VARS ONLY:
-
-# // SP-specific uncategorized vars
-# $border-radius-default-SP: 3px;
-# $font-weight-medium-bold-SP: 500;
-
-
-# // Primary color variables
-
-# $gray-lighter-SP: #d9d9d9; // used also for borders
-# $gray-light-SP: #929292;
-# $gray-sitepoint-header-bg-SP: #3A3A3A;
-# $gray-sitepoint-header-SP: #ADADAD;
-# $gray-text-SP: #6d6d6d;
-# $gray-bg-SP: #f5f5f5; // used for gray backgrounds
-
-# $blue-SP: #0582b6; // hex from Pete (used for links etc.)
-
-# $red-SP: #ec5645;
-
-# $green-SP: #25c65e;
-
-
-# // Primary button
-# $btn-primary-border-color: $red-SP !default;
-# $btn-primary-background-color: $red-SP !default;
-# $btn-primary-background-color-dark: darken($red-SP, 10%) !default;
-# $btn-primary-background-color-light: lighten($red-SP, 10%) !default;
-
-# // Pill nav
-
-# $nav-pills-color: $gray-text-SP;
-
-# $nav-pills-background-color-active: $red-SP;
-
-# // Stacked nav
-
-# $nav-stacked-color: $gray-text-SP;
-
-# $nav-stacked-background-color-active: $red-SP;
-
-
-
-# // Broad general variables
-
-# $primary_border_color: $gray-lighter-SP;
-
-# $link_color:  $blue-SP;
-
-# $link-color-visited: darken($blue-SP, 10%);
-# $link-color-hover: darken($blue-SP, 10%);
-# $link-color-active: darken($blue-SP, 10%);
-
-
-# @import "desktop";
-
-
-# body {
-#     // background-color: #00aff0;
-# }
