7265c97d94caa45a4756f10cc1e59afc87c4f075
Boris <boris.kvid@sitepointgroup.com>
2014-06-27 17:17:51 +1000

    Trello #367 Discourse theme - align with upstream tag v0.9.9.10 + create mobile theme: topic polish (TODO: down icon in quoted post has no padding)

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,14 @@
 # e66c7f81a36078606a66c45519e96d5f61ee44d6
 # 20 June 2014 12:18:58 am AEST
 
+# When styles are not working or are not updating, try:
+# - stopping server
+# - sitepoint/discourse rm -rf tmp
+# - delete sitepoint/discourse/public/uploads/stylesheet-cache
+# - restart server
+# If styles are still not updating, there is probably a syntax error in the SCSS causing a silent failure and causing the file not being processed.
+# To be 100% sure you can also enable Chrome Dev Tools -> Settings -> General -> Disable cache (while DevTools is open), but note it leads to 30s onload times.
+
 after_initialize do
 
   module SitepointDesign
