ae72dfb43eb2be5ec466283638e1915906ca0ba5
David Li <david.li@sitepoint.com>
2015-08-28 12:21:58 +1000

    Add Alexa Claim & Certify customisation

--- plugin.rb
+++ plugin.rb
@@ -47,6 +47,14 @@ after_initialize do
       <a href="https://learnable.com/topics/all/book">Books</a>
       <a href="https://learnable.com/topics/all/course">Courses</a>
     </noscript>
+
+    <!-- Start Alexa Certify Javascript -->
+    <script type="text/javascript">
+    _atrk_opts = { atrk_acct:"3/2Rk1ao6C526C", domain:"sitepoint.com",dynamic: true};
+    (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+    </script>
+    <noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=3/2Rk1ao6C526C" style="display:none" height="1" width="1" alt="" /></noscript>
+    <!-- End Alexa Certify Javascript -->
     EOS
 
   begin
